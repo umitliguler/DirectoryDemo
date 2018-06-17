a95d40cd72dce6197df8a9c15f9fc9612faac48b
umitliguler <info.umitguler@gmail.com>
2018-06-17 11:11:41 +0300

    first

---
+++ DirectoryDemo/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DirectoryDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Directory.CreateDirectory("c:\\vidobu\\test1");
+            Console.WriteLine(Directory.GetCreationTime("c:\\vidobu\\test1"));
+
+            Directory.CreateDirectory("c:\\vidobu\\test2");
+            Console.WriteLine(Directory.GetCreationTime("c:\\vidobu\\test2"));
+
+            Directory.Move("c:\\vidobu\\test1", "c:\\vidobu\\test2\\test15");
+
+            //if (Directory.Exists("c:\\vidobu\\test1"))
+            //{
+            //    Directory.Delete("c:\\vidobu\\test1"); 
+            //}
+
+            Console.ReadKey();
+        }
+    }
+}
